ea42e19114673d45db9f1089d4b6201649f273c8
gajjar.s <gajjar.s@husky.neu.edu>
2018-09-14 14:43:54 -0400

    1. naming refactoring in SimpleFibonacciCounter 2. added method getNthFibonacciNumber 3. added logic to increase/decrement Counter and throw exception if overflow or underflow occurs

--- Labs/PDP-Lab2/src/fib/SimpleFibonacciCounter.java
+++ Labs/PDP-Lab2/src/fib/SimpleFibonacciCounter.java
@@ -2,36 +2,66 @@ package fib;
 
 public class SimpleFibonacciCounter implements FibonacciCounter {
 
+  private static final long FIRST_FIBONACCI_NUMBER = 0;
+  private static final long SECOND_FIBONACCI_NUMBER = 1;
+
   private final long currentCount;
-  private final long currentValue;
+  private final long currentFibonacciValue;
 
   public SimpleFibonacciCounter() {
-    currentValue = 0;
+    currentFibonacciValue = 0;
     currentCount = 1;
   }
 
-  public SimpleFibonacciCounter(long currentCount, long currentValue) {
+  public SimpleFibonacciCounter(long currentCount, long currentFibonacciValue) {
     this.currentCount = currentCount;
-    this.currentValue = currentValue;
+    this.currentFibonacciValue = currentFibonacciValue;
   }
 
   @Override
-  public FibonacciCounter incrementCounter() {
-    return null;
+  public FibonacciCounter incrementCounter() throws ArithmeticException {
+    long newCount = Math.addExact(this.currentCount, 1);
+    long newFibonacciValue = getNthFibonacciNumber(newCount);
+    return new SimpleFibonacciCounter(newCount, newFibonacciValue);
   }
 
   @Override
-  public FibonacciCounter decrementCounter() {
-    return null;
+  public FibonacciCounter decrementCounter() throws ArithmeticException {
+    long newCount = Math.addExact(this.currentCount, -1);
+    long newFibonacciValue = getNthFibonacciNumber(newCount);
+    return new SimpleFibonacciCounter(newCount, newFibonacciValue);
   }
 
   @Override
   public long getCurrentCount() {
-    return 0;
+    return this.currentCount;
   }
 
   @Override
   public long getCurrentFibonacciValue() {
-    return 0;
+    return this.currentFibonacciValue;
+  }
+
+  private long getNthFibonacciNumber(long n) throws ArithmeticException {
+    if (n == 1) {
+      return FIRST_FIBONACCI_NUMBER;
+    } else if (n == 2) {
+      return SECOND_FIBONACCI_NUMBER;
+    } else {
+
+      long first = FIRST_FIBONACCI_NUMBER;
+      long second = SECOND_FIBONACCI_NUMBER;
+      long sum = 0;
+      for (int i = 2; i <= n; i++) {
+        sum = Math.addExact(first, second);
+        second = first;
+        first = sum;
+      }
+      return sum;
+    }
+  }
+
+  public static void main(String[] args) {
+    Math.addExact(1, Long.MAX_VALUE);
   }
 }
