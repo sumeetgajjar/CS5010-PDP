acb73290f781bb594738f36c550e4a254e6e69d5
gajjar.s <gajjar.s@husky.neu.edu>
2018-09-14 17:33:24 -0400

    added javadoc for the class

--- Labs/PDP-Lab2/src/fib/FibonacciCounter.java
+++ Labs/PDP-Lab2/src/fib/FibonacciCounter.java
@@ -1,5 +1,9 @@
 package fib;
 
+/**
+ * This interface represents a set of operations for a FibonacciCounter. A Fibonacci counter counts
+ * Fibonacci numbers. The count starts at 1 (the first Fibonacci number is 0, the second is 1).
+ */
 public interface FibonacciCounter {
 
   FibonacciCounter incrementCounter() throws ArithmeticException;
