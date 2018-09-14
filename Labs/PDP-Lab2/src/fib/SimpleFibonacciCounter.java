8ccade91fe87292d1000cccc5e4314e95454bfbc
gajjar.s <gajjar.s@husky.neu.edu>
2018-09-14 14:10:00 -0400

    added blank class SimpleFibonacciCounter which implements Fibonacci Counter

---
+++ Labs/PDP-Lab2/src/fib/SimpleFibonacciCounter.java
@@ -0,0 +1,24 @@
+package fib;
+
+public class SimpleFibonacciCounter implements FibonacciCounter {
+
+  @Override
+  public FibonacciCounter incrmentCounter() {
+    return null;
+  }
+
+  @Override
+  public FibonacciCounter decrementCounter() {
+    return null;
+  }
+
+  @Override
+  public int getCurrentCount() {
+    return 0;
+  }
+
+  @Override
+  public int getCurrentFibonnaciValue() {
+    return 0;
+  }
+}
