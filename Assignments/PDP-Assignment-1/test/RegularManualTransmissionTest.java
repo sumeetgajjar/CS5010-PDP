1290b47326ecac80fdec778fe80891dbc2777c26
gajjar.s <gajjar.s@husky.neu.edu>
2018-09-14 19:55:47 -0400

    added javadoc comments for RegularManualTransmissionTest

--- Assignments/PDP-Assignment-1/test/RegularManualTransmissionTest.java
+++ Assignments/PDP-Assignment-1/test/RegularManualTransmissionTest.java
@@ -4,6 +4,9 @@ import org.junit.Test;
 import vehicle.ManualTransmission;
 import vehicle.RegularManualTransmission;
 
+/**
+ * A JUnit class to test RegularManualTransmission.
+ */
 public class RegularManualTransmissionTest {
 
   @Test
