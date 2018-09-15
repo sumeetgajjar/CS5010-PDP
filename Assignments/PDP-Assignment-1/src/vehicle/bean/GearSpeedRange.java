dbe0fab2c226c65d4a44cf388db55705d9a430cb
gajjar.s <gajjar.s@husky.neu.edu>
2018-09-15 01:09:27 -0400

    Added javadoc comments for GearSpeedRange class

--- Assignments/PDP-Assignment-1/src/vehicle/bean/GearSpeedRange.java
+++ Assignments/PDP-Assignment-1/src/vehicle/bean/GearSpeedRange.java
@@ -8,15 +8,31 @@ public class GearSpeedRange {
   private final int lowerLimit;
   private final int upperLimit;
 
+  /**
+   * Constructs the GearSpeedRange object with given lower and upper limit of speed.
+   *
+   * @param lowerLimit lower limit of the gear speed range
+   * @param upperLimit upper limit of the gear speed range
+   */
   public GearSpeedRange(int lowerLimit, int upperLimit) {
     this.lowerLimit = lowerLimit;
     this.upperLimit = upperLimit;
   }
 
+  /**
+   * Returns the lower limit of the gear speed range.
+   *
+   * @return the lower limit of the gear speed range.
+   */
   public int getLowerLimit() {
     return lowerLimit;
   }
 
+  /**
+   * Returns the upper limit of the gear speed range.
+   *
+   * @return the upper limit of the gear speed range.
+   */
   public int getUpperLimit() {
     return upperLimit;
   }
