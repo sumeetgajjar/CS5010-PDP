bff527cdd204b5621beb0eba531c9e0143ad92e7
gajjar.s <gajjar.s@husky.neu.edu>
2018-09-23 18:54:32 -0400

    1. added method to check if the input character is legal

--- Assignments/PDP-Assignment-2/src/calculator/SimpleCalculator.java
+++ Assignments/PDP-Assignment-2/src/calculator/SimpleCalculator.java
@@ -1,10 +1,46 @@
 package calculator;
 
-public class SimpleCalculator implements Calculator {
+import calculator.bean.InputType;
+
+public class SimpleCalculator extends AbstractCalculator {
+
+  private int firstOperand;
+  private int secondOperand;
+  private String result = "";
+
 
   @Override
-  public Calculator input(char input) {
+  public Calculator input(char input) throws IllegalArgumentException {
+    isInputCharacterLegal(input);
+
+    InputType inputType = getInputType(input);
+
+
     return new SimpleCalculator();
+  }
+
+  private void isInputCharacterLegal(char input) throws IllegalArgumentException {
+    boolean isInputLegal = false;
+
+    char[] legalInputs = getSupportedInputs();
+    for (char legalInput : legalInputs) {
+      if (input == legalInput) {
+        isInputLegal = true;
+        break;
+      }
+    }
+
+    if (!isInputLegal) {
+      throw new IllegalArgumentException(String.format("Input: %s is illegal", input));
+    }
+  }
+
+  private char[] getSupportedInputs() {
+    return new char[]{
+            '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+            '+', '-', '*',
+            'C'
+    };
   }
 
   @Override
