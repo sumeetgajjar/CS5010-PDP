56daa4a6b9d911507d43535dc2a384d317ab6e8b
gajjar.s <gajjar.s@husky.neu.edu>
2018-09-23 03:21:27 -0400

    moved more methods from SimpleCalculator to base class

--- Assignments/PDP-Assignment-2/test/AbstractCalculatorTest.java
+++ Assignments/PDP-Assignment-2/test/AbstractCalculatorTest.java
@@ -191,4 +191,105 @@ public abstract class AbstractCalculatorTest {
             .input('+').input('1').input('1');
     Assert.assertEquals("11", calculator2.getResult());
   }
+
+  @Test
+  public void testMultipleOperators() {
+    List<Pair<Character, String>> testSequences = new ArrayList<>();
+    testSequences.add(Pair.of('1', "1"));
+    testSequences.add(Pair.of('0', "10"));
+    testSequences.add(Pair.of('+', "10+"));
+    testSequences.add(Pair.of('2', "10+2"));
+    testSequences.add(Pair.of('-', "10+2-"));
+    testSequences.add(Pair.of('4', "10+2-4"));
+    testSequences.add(Pair.of('*', "10+2-4*"));
+    testSequences.add(Pair.of('3', "10+2-4*3"));
+    testSequences.add(Pair.of('+', "10+2-4*3+"));
+    testSequences.add(Pair.of('1', "10+2-4*3+1"));
+    testSequences.add(Pair.of('=', "25"));
+
+    executeSequencesAndVerifyResult(testSequences);
+  }
+
+  @Test
+  public void testBasicOperations() {
+    List<Pair<Character, String>> oneDigitTestSequences = new ArrayList<>();
+    oneDigitTestSequences.add(Pair.of('8', "8"));
+    oneDigitTestSequences.add(Pair.of('+', "8+"));
+    oneDigitTestSequences.add(Pair.of('2', "8+2"));
+    oneDigitTestSequences.add(Pair.of('=', "10"));
+
+    oneDigitTestSequences.add(Pair.of('-', "10-"));
+    oneDigitTestSequences.add(Pair.of('4', "10-4"));
+    oneDigitTestSequences.add(Pair.of('=', "6"));
+
+    oneDigitTestSequences.add(Pair.of('*', "6*"));
+    oneDigitTestSequences.add(Pair.of('2', "6*2"));
+    oneDigitTestSequences.add(Pair.of('=', "12"));
+
+    executeSequencesAndVerifyResult(oneDigitTestSequences);
+
+    List<Pair<Character, String>> twoDigitTestSequences = new ArrayList<>();
+    twoDigitTestSequences.add(Pair.of('1', "1"));
+    twoDigitTestSequences.add(Pair.of('2', "12"));
+    twoDigitTestSequences.add(Pair.of('+', "12+"));
+    twoDigitTestSequences.add(Pair.of('1', "12+1"));
+    twoDigitTestSequences.add(Pair.of('0', "12+10"));
+    twoDigitTestSequences.add(Pair.of('=', "22"));
+
+    twoDigitTestSequences.add(Pair.of('-', "22-"));
+    twoDigitTestSequences.add(Pair.of('2', "22-2"));
+    twoDigitTestSequences.add(Pair.of('2', "22-22"));
+    twoDigitTestSequences.add(Pair.of('=', "0"));
+
+    twoDigitTestSequences.add(Pair.of('*', "0*"));
+    twoDigitTestSequences.add(Pair.of('1', "0*1"));
+    twoDigitTestSequences.add(Pair.of('0', "0*10"));
+    twoDigitTestSequences.add(Pair.of('=', "0"));
+
+    executeSequencesAndVerifyResult(twoDigitTestSequences);
+
+    List<Pair<Character, String>> threeDigitTestSequences = new ArrayList<>();
+    threeDigitTestSequences.add(Pair.of('1', "1"));
+    threeDigitTestSequences.add(Pair.of('2', "12"));
+    threeDigitTestSequences.add(Pair.of('3', "123"));
+    threeDigitTestSequences.add(Pair.of('+', "123+"));
+    threeDigitTestSequences.add(Pair.of('1', "123+1"));
+    threeDigitTestSequences.add(Pair.of('0', "123+10"));
+    threeDigitTestSequences.add(Pair.of('0', "123+100"));
+    threeDigitTestSequences.add(Pair.of('=', "223"));
+
+    threeDigitTestSequences.add(Pair.of('-', "223-"));
+    threeDigitTestSequences.add(Pair.of('2', "223-2"));
+    threeDigitTestSequences.add(Pair.of('0', "223-20"));
+    threeDigitTestSequences.add(Pair.of('0', "223-200"));
+    threeDigitTestSequences.add(Pair.of('=', "23"));
+
+    threeDigitTestSequences.add(Pair.of('*', "23*"));
+    threeDigitTestSequences.add(Pair.of('1', "23*1"));
+    threeDigitTestSequences.add(Pair.of('0', "23*10"));
+    threeDigitTestSequences.add(Pair.of('0', "23*100"));
+    threeDigitTestSequences.add(Pair.of('=', "2300"));
+
+    executeSequencesAndVerifyResult(threeDigitTestSequences);
+
+    List<Pair<Character, String>> clearInputTestSequences = new ArrayList<>();
+    clearInputTestSequences.add(Pair.of('C', ""));
+    clearInputTestSequences.add(Pair.of('C', ""));
+    clearInputTestSequences.add(Pair.of('C', ""));
+    clearInputTestSequences.add(Pair.of('1', "1"));
+    clearInputTestSequences.add(Pair.of('C', ""));
+    clearInputTestSequences.add(Pair.of('1', "1"));
+    clearInputTestSequences.add(Pair.of('-', "1-"));
+    clearInputTestSequences.add(Pair.of('C', ""));
+    clearInputTestSequences.add(Pair.of('4', "4"));
+    clearInputTestSequences.add(Pair.of('-', "4-"));
+    clearInputTestSequences.add(Pair.of('2', "4-2"));
+    clearInputTestSequences.add(Pair.of('=', "2"));
+    clearInputTestSequences.add(Pair.of('C', ""));
+    clearInputTestSequences.add(Pair.of('C', ""));
+    clearInputTestSequences.add(Pair.of('C', ""));
+    clearInputTestSequences.add(Pair.of('C', ""));
+
+    executeSequencesAndVerifyResult(clearInputTestSequences);
+  }
 }
--- Assignments/PDP-Assignment-2/test/SimpleCalculatorTest.java
+++ Assignments/PDP-Assignment-2/test/SimpleCalculatorTest.java
@@ -1,36 +1,14 @@
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import calculator.Calculator;
 import calculator.SimpleCalculator;
-import calculator.bean.Pair;
 
 public class SimpleCalculatorTest extends AbstractCalculatorTest {
 
   @Override
   protected SimpleCalculator getCalculatorInstance() {
     return new SimpleCalculator();
-  }
-
-  @Test
-  public void testMultipleOperators() {
-    List<Pair<Character, String>> testSequences = new ArrayList<>();
-    testSequences.add(Pair.of('1', "1"));
-    testSequences.add(Pair.of('0', "10"));
-    testSequences.add(Pair.of('+', "10+"));
-    testSequences.add(Pair.of('2', "10+2"));
-    testSequences.add(Pair.of('-', "10+2-"));
-    testSequences.add(Pair.of('4', "10+2-4"));
-    testSequences.add(Pair.of('*', "10+2-4*"));
-    testSequences.add(Pair.of('3', "10+2-4*3"));
-    testSequences.add(Pair.of('+', "10+2-4*3+"));
-    testSequences.add(Pair.of('1', "10+2-4*3+1"));
-    testSequences.add(Pair.of('=', "25"));
-
-    executeSequencesAndVerifyResult(testSequences);
   }
 
   @Test
@@ -128,89 +106,6 @@ public class SimpleCalculatorTest extends AbstractCalculatorTest {
     } catch (IllegalArgumentException ignored) {
     }
     Assert.assertEquals("32+", calculator.getResult());
-  }
-
-  @Test
-  public void testBasicOperations() {
-    List<Pair<Character, String>> oneDigitTestSequences = new ArrayList<>();
-    oneDigitTestSequences.add(Pair.of('8', "8"));
-    oneDigitTestSequences.add(Pair.of('+', "8+"));
-    oneDigitTestSequences.add(Pair.of('2', "8+2"));
-    oneDigitTestSequences.add(Pair.of('=', "10"));
-
-    oneDigitTestSequences.add(Pair.of('-', "10-"));
-    oneDigitTestSequences.add(Pair.of('4', "10-4"));
-    oneDigitTestSequences.add(Pair.of('=', "6"));
-
-    oneDigitTestSequences.add(Pair.of('*', "6*"));
-    oneDigitTestSequences.add(Pair.of('2', "6*2"));
-    oneDigitTestSequences.add(Pair.of('=', "12"));
-
-    executeSequencesAndVerifyResult(oneDigitTestSequences);
-
-    List<Pair<Character, String>> twoDigitTestSequences = new ArrayList<>();
-    twoDigitTestSequences.add(Pair.of('1', "1"));
-    twoDigitTestSequences.add(Pair.of('2', "12"));
-    twoDigitTestSequences.add(Pair.of('+', "12+"));
-    twoDigitTestSequences.add(Pair.of('1', "12+1"));
-    twoDigitTestSequences.add(Pair.of('0', "12+10"));
-    twoDigitTestSequences.add(Pair.of('=', "22"));
-
-    twoDigitTestSequences.add(Pair.of('-', "22-"));
-    twoDigitTestSequences.add(Pair.of('2', "22-2"));
-    twoDigitTestSequences.add(Pair.of('2', "22-22"));
-    twoDigitTestSequences.add(Pair.of('=', "0"));
-
-    twoDigitTestSequences.add(Pair.of('*', "0*"));
-    twoDigitTestSequences.add(Pair.of('1', "0*1"));
-    twoDigitTestSequences.add(Pair.of('0', "0*10"));
-    twoDigitTestSequences.add(Pair.of('=', "0"));
-
-    executeSequencesAndVerifyResult(twoDigitTestSequences);
-
-    List<Pair<Character, String>> threeDigitTestSequences = new ArrayList<>();
-    threeDigitTestSequences.add(Pair.of('1', "1"));
-    threeDigitTestSequences.add(Pair.of('2', "12"));
-    threeDigitTestSequences.add(Pair.of('3', "123"));
-    threeDigitTestSequences.add(Pair.of('+', "123+"));
-    threeDigitTestSequences.add(Pair.of('1', "123+1"));
-    threeDigitTestSequences.add(Pair.of('0', "123+10"));
-    threeDigitTestSequences.add(Pair.of('0', "123+100"));
-    threeDigitTestSequences.add(Pair.of('=', "223"));
-
-    threeDigitTestSequences.add(Pair.of('-', "223-"));
-    threeDigitTestSequences.add(Pair.of('2', "223-2"));
-    threeDigitTestSequences.add(Pair.of('0', "223-20"));
-    threeDigitTestSequences.add(Pair.of('0', "223-200"));
-    threeDigitTestSequences.add(Pair.of('=', "23"));
-
-    threeDigitTestSequences.add(Pair.of('*', "23*"));
-    threeDigitTestSequences.add(Pair.of('1', "23*1"));
-    threeDigitTestSequences.add(Pair.of('0', "23*10"));
-    threeDigitTestSequences.add(Pair.of('0', "23*100"));
-    threeDigitTestSequences.add(Pair.of('=', "2300"));
-
-    executeSequencesAndVerifyResult(threeDigitTestSequences);
-
-    List<Pair<Character, String>> clearInputTestSequences = new ArrayList<>();
-    clearInputTestSequences.add(Pair.of('C', ""));
-    clearInputTestSequences.add(Pair.of('C', ""));
-    clearInputTestSequences.add(Pair.of('C', ""));
-    clearInputTestSequences.add(Pair.of('1', "1"));
-    clearInputTestSequences.add(Pair.of('C', ""));
-    clearInputTestSequences.add(Pair.of('1', "1"));
-    clearInputTestSequences.add(Pair.of('-', "1-"));
-    clearInputTestSequences.add(Pair.of('C', ""));
-    clearInputTestSequences.add(Pair.of('4', "4"));
-    clearInputTestSequences.add(Pair.of('-', "4-"));
-    clearInputTestSequences.add(Pair.of('2', "4-2"));
-    clearInputTestSequences.add(Pair.of('=', "2"));
-    clearInputTestSequences.add(Pair.of('C', ""));
-    clearInputTestSequences.add(Pair.of('C', ""));
-    clearInputTestSequences.add(Pair.of('C', ""));
-    clearInputTestSequences.add(Pair.of('C', ""));
-
-    executeSequencesAndVerifyResult(clearInputTestSequences);
   }
 
 }
