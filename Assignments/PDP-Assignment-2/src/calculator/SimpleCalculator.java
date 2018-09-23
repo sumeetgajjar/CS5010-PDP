c50fc9b8631993b2d555ec264c5d9bed6a223bba
gajjar.s <gajjar.s@husky.neu.edu>
2018-09-23 19:27:25 -0400

    added method to check and identify input type

--- Assignments/PDP-Assignment-2/src/calculator/SimpleCalculator.java
+++ Assignments/PDP-Assignment-2/src/calculator/SimpleCalculator.java
@@ -2,21 +2,26 @@ package calculator;
 
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
+import java.util.LinkedHashSet;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import calculator.bean.InputType;
+
 public class SimpleCalculator extends AbstractCalculator {
 
-  private static final List<Character> SUPPORTED_DIGITS =
-          Collections.unmodifiableList(
-                  Arrays.asList('1', '2', '3', '4', '5', '6', '7', '8', '9', '0'));
+  private static final Set<Character> SUPPORTED_DIGITS =
+          Collections.unmodifiableSet(new LinkedHashSet<>(
+                  Arrays.asList('1', '2', '3', '4', '5', '6', '7', '8', '9', '0')));
 
-  private static final List<Character> SUPPORTED_OPERATORS =
-          Collections.unmodifiableList(Arrays.asList('+', '-', '*'));
+  private static final Set<Character> SUPPORTED_OPERATORS =
+          Collections.unmodifiableSet(new LinkedHashSet<>(
+                  Arrays.asList('+', '-', '*')
+          ));
 
-  private static final List<Character> SUPPORTED_MISC_CHARACTERS =
-          Collections.unmodifiableList(Arrays.asList('=', 'C'));
+  private static final char CLEAR_INPUT_CHARACTER = 'C';
+  private static final char EQUAL_TO_CHARACTER = '=';
 
 
   @Override
@@ -27,45 +32,47 @@ public class SimpleCalculator extends AbstractCalculator {
   @Override
   public Calculator input(char input) throws IllegalArgumentException {
     isInputCharacterLegal(input);
+    InputType inputType = getInputType(input);
 
-//    InputType inputType = getInputType(input);
 
+    return null;
+  }
 
-    return new SimpleCalculator();
+  private InputType getInputType(char input) throws IllegalArgumentException {
+    if (getSupportedDigits().contains(input)) {
+      return InputType.OPERAND;
+    } else if (getSupportedOperators().contains(input)) {
+      return InputType.OPERATOR;
+    } else if (CLEAR_INPUT_CHARACTER == input) {
+      return InputType.CLEAR;
+    } else if (EQUAL_TO_CHARACTER == input) {
+      return InputType.EQUAL_TO;
+    } else {
+      throw new IllegalArgumentException(String.format("Cannot Identify the InputType for:%s", input));
+    }
   }
 
   private void isInputCharacterLegal(char input) throws IllegalArgumentException {
-    boolean isInputLegal = false;
-
-    List<Character> legalInputs = getSupportedInputs();
-
-    for (char legalInput : legalInputs) {
-      if (input == legalInput) {
-        isInputLegal = true;
-        break;
-      }
-    }
-
-    if (!isInputLegal) {
+    Set<Character> legalInputSet = getSupportedInputs();
+    if (!legalInputSet.contains(input)) {
       throw new IllegalArgumentException(String.format("Input: %s is illegal", input));
     }
   }
 
-  private List<Character> getSupportedInputs() {
-    return Stream.of(getSupportedDigits(), getSupportedOperators(), getSupportedMiscCharacters())
-            .flatMap(List::stream)
-            .collect(Collectors.toList());
+  private Set<Character> getSupportedInputs() {
+    return Stream.of(getSupportedDigits(),
+            getSupportedOperators(),
+            Collections.singleton(CLEAR_INPUT_CHARACTER),
+            Collections.singleton(EQUAL_TO_CHARACTER))
+            .flatMap(Set::stream)
+            .collect(Collectors.toCollection(LinkedHashSet::new));
   }
 
-  private List<Character> getSupportedDigits() {
+  private Set<Character> getSupportedDigits() {
     return SUPPORTED_DIGITS;
   }
 
-  private List<Character> getSupportedOperators() {
+  private Set<Character> getSupportedOperators() {
     return SUPPORTED_OPERATORS;
-  }
-
-  private List<Character> getSupportedMiscCharacters() {
-    return SUPPORTED_MISC_CHARACTERS;
   }
 }
