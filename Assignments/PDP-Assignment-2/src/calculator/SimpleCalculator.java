30c9cac28f1521836f9bf73cee2196e518827465
gajjar.s <gajjar.s@husky.neu.edu>
2018-09-23 21:55:46 -0400

    1. added method to validate the input character

--- Assignments/PDP-Assignment-2/src/calculator/SimpleCalculator.java
+++ Assignments/PDP-Assignment-2/src/calculator/SimpleCalculator.java
@@ -2,12 +2,13 @@ package calculator;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import calculator.bean.InputType;
+import calculator.bean.InputCategory;
 
 public class SimpleCalculator extends AbstractCalculator {
 
@@ -23,6 +24,7 @@ public class SimpleCalculator extends AbstractCalculator {
   private static final char CLEAR_INPUT_CHARACTER = 'C';
   private static final char EQUAL_TO_CHARACTER = '=';
 
+  private final String inputSequence = "";
 
   @Override
   public String getResult() {
@@ -32,23 +34,53 @@ public class SimpleCalculator extends AbstractCalculator {
   @Override
   public Calculator input(char input) throws IllegalArgumentException {
     isInputCharacterLegal(input);
-    InputType inputType = getInputType(input);
-
+    InputCategory paramInputCategory = getInputType(input);
+    isInputCharacterValid(paramInputCategory);
+    String newInputSequence = inputSequence + input;
 
     return null;
   }
 
-  private InputType getInputType(char input) throws IllegalArgumentException {
-    if (getSupportedDigits().contains(input)) {
-      return InputType.OPERAND;
-    } else if (getSupportedOperators().contains(input)) {
-      return InputType.OPERATOR;
-    } else if (CLEAR_INPUT_CHARACTER == input) {
-      return InputType.CLEAR;
-    } else if (EQUAL_TO_CHARACTER == input) {
-      return InputType.EQUAL_TO;
+  private void isInputCharacterValid(InputCategory paramInputCategory) throws IllegalArgumentException {
+    if (inputSequence.isEmpty()) {
+      if (paramInputCategory == InputCategory.EQUAL_TO || paramInputCategory == InputCategory.OPERATOR) {
+        throw new IllegalArgumentException("Invalid Input");
+      }
     } else {
-      throw new IllegalArgumentException(String.format("Cannot Identify the InputType for:%s", input));
+      InputCategory currentInputCategory = getInputType(inputSequence.charAt(inputSequence.length() - 1));
+      Set<InputCategory> illegalInputCategorySet = getIllegalInputCategory(currentInputCategory);
+      if (illegalInputCategorySet.contains(paramInputCategory)) {
+        throw new IllegalArgumentException("Invalid Input");
+      }
+    }
+  }
+
+  private Set<InputCategory> getIllegalInputCategory(InputCategory inputCategory) {
+    switch (inputCategory) {
+      case OPERAND:
+        return Collections.emptySet();
+      case OPERATOR:
+        return Collections.unmodifiableSet(new HashSet<>(Arrays.asList(InputCategory.OPERATOR, InputCategory.EQUAL_TO)));
+      case EQUAL_TO:
+        return Collections.unmodifiableSet(new HashSet<>(Arrays.asList(InputCategory.EQUAL_TO, InputCategory.OPERAND)));
+      case CLEAR:
+        return Collections.unmodifiableSet(new HashSet<>(Arrays.asList(InputCategory.OPERATOR, InputCategory.EQUAL_TO)));
+      default:
+        throw new IllegalStateException("Invalid InputCategory");
+    }
+  }
+
+  private InputCategory getInputType(char input) throws IllegalArgumentException {
+    if (getSupportedDigits().contains(input)) {
+      return InputCategory.OPERAND;
+    } else if (getSupportedOperators().contains(input)) {
+      return InputCategory.OPERATOR;
+    } else if (CLEAR_INPUT_CHARACTER == input) {
+      return InputCategory.CLEAR;
+    } else if (EQUAL_TO_CHARACTER == input) {
+      return InputCategory.EQUAL_TO;
+    } else {
+      throw new IllegalArgumentException(String.format("Cannot Identify the InputCategory for:%s", input));
     }
   }
 
