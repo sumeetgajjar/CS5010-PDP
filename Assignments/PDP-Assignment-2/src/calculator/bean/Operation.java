070381f0be53c43237dc20a67fa19ccbee39563e
gajjar.s <gajjar.s@husky.neu.edu>
2018-09-26 16:58:04 -0400

    added javadoc comments for methods in Operation.java

--- Assignments/PDP-Assignment-2/src/calculator/bean/Operation.java
+++ Assignments/PDP-Assignment-2/src/calculator/bean/Operation.java
@@ -1,22 +1,60 @@
 package calculator.bean;
 
+/**
+ * This enum represents Set of arithmetic operations.
+ */
 public enum Operation {
 
+  /**
+   * Represents addition operation.
+   */
   ADD('+') {
+    /**
+     * Returns the sum of given numbers and throws exception if overflow occurs.
+     *
+     * @param n1 number 1
+     * @param n2 number 2
+     * @return the sum of given numbers
+     * @throws ArithmeticException if operation results in 32-bit overflow occurs
+     */
     @Override
-    public int perform(int n1, int n2) {
+    public int perform(int n1, int n2) throws ArithmeticException {
       return Math.addExact(n1, n2);
     }
   },
+
+  /**
+   * Represents subtraction operation.
+   */
   SUBTRACT('-') {
+    /**
+     * Returns the difference of given numbers and throws exception if overflow occurs.
+     *
+     * @param n1 number1
+     * @param n2 number2
+     * @return the difference of given numbers
+     * @throws ArithmeticException if operation results in 32-bit overflow occurs
+     */
     @Override
-    public int perform(int n1, int n2) {
+    public int perform(int n1, int n2) throws ArithmeticException {
       return Math.subtractExact(n1, n2);
     }
   },
+
+  /**
+   * Represents multiplication operation.
+   */
   MULTIPLY('*') {
+    /**
+     * Returns the product of given numbers and throws exception if overflow occurs.
+     *
+     * @param n1 number1
+     * @param n2 number2
+     * @return the producr of given numbers
+     * @throws ArithmeticException if operation results in 32-bit overflow occurs
+     */
     @Override
-    public int perform(int n1, int n2) {
+    public int perform(int n1, int n2) throws ArithmeticException {
       return Math.multiplyExact(n1, n2);
     }
   };
@@ -27,12 +65,31 @@ public enum Operation {
     this.symbol = symbol;
   }
 
+  /**
+   * Returns the result of arithmetic operation on given numbers.
+   *
+   * @param n1 number1
+   * @param n2 number2
+   * @return the result of arithmetic operation on given numbers
+   * @throws ArithmeticException if operation results in 32-bit overflow occurs
+   */
   public abstract int perform(int n1, int n2) throws ArithmeticException;
 
+  /**
+   * Returns the symbol associated with the operation.
+   *
+   * @return the symbol associated with the operation
+   */
   public char getSymbol() {
     return symbol;
   }
 
+  /**
+   * Returns the Operation associated with the symbol.
+   *
+   * @param symbol symbol
+   * @return the Operation associated with the symbol
+   */
   public static Operation getOperation(char symbol) {
     for (Operation operation : Operation.values()) {
       if (operation.getSymbol() == symbol) {
