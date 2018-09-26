448b7d606064112d9bb849bd4a9f9bfa41dfe8e2
gajjar.s <gajjar.s@husky.neu.edu>
2018-09-26 17:09:41 -0400

    added javadoc comments for AbstractCommand

--- Assignments/PDP-Assignment-2/src/calculator/command/AbstractCommand.java
+++ Assignments/PDP-Assignment-2/src/calculator/command/AbstractCommand.java
@@ -4,6 +4,25 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * This class provides a skeletal implementation of the {@link Command} interface. It minimize the
+ * effort required to implement this interface. It has following protected fields.
+ * <ul>
+ * <li><code>expression</code>: current algebraic expression</li>
+ * <li><code>supportedCharacters</code>: characters supported by the command</li>
+ * </ul>
+ *
+ * To implement a {@link Command} the programmer needs to extend this class and provide
+ * implementations for following methods.
+ *
+ * <ul>
+ * <li>getCommandName</li>
+ * <li>performAction</li>
+ * <li>getNextValidCommands</li>
+ * </ul>
+ * The programmer should also provide a constructor, of following signature Command({@link List}
+ * expression, {@link Set} supportedCharacters)
+ */
 public abstract class AbstractCommand implements Command {
 
   protected final List<String> expression;
