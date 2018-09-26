4295c3c2e6dc53a88c4eec7dc72e1dea21cbb56a
gajjar.s <gajjar.s@husky.neu.edu>
2018-09-26 16:52:17 -0400

    added javadoc comments for methods in SmartCalculator.java

--- Assignments/PDP-Assignment-2/src/calculator/bean/CommandName.java
+++ Assignments/PDP-Assignment-2/src/calculator/bean/CommandName.java
@@ -1,8 +1,26 @@
 package calculator.bean;
 
+/**
+ * This enum represents the Name of the Command an input can have.
+ */
 public enum CommandName {
+  /**
+   * If the input is operand. e.g. "0", "1" and so on.
+   */
   OPERAND,
+
+  /**
+   * If the input is operator. e.g. "+", "-" and so on.
+   */
   OPERATOR,
+
+  /**
+   * If the input is equal to. e.g. "=".
+   */
   EQUAL_TO,
+
+  /**
+   * If the input is clear. e.g. "C".
+   */
   CLEAR
 }
