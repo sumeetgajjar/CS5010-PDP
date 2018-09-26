fc27d50fd42779981c49f6158403b9e7d5cb151f
gajjar.s <gajjar.s@husky.neu.edu>
2018-09-26 15:02:07 -0400

    added javadoc comments for Utils class

--- Assignments/PDP-Assignment-2/src/calculator/util/Utils.java
+++ Assignments/PDP-Assignment-2/src/calculator/util/Utils.java
@@ -4,12 +4,27 @@ import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * This class represents set of util functions which can be used by any class.
+ */
 public class Utils {
 
+  /**
+   * Returns a {@link Deque} of the specified {@link List}.
+   *
+   * @param expressions the List for which the Deque is to be returned
+   * @return the Deque of the specified List
+   */
   public static Deque<String> getExpressionDeque(List<String> expressions) {
     return new LinkedList<>(expressions);
   }
 
+  /**
+   * Returns a {@link List} of the specified {@link Deque}.
+   *
+   * @param expressionDeque the Deque for which the List is to be returned
+   * @return the List of the specified Deque
+   */
   public static List<String> getExpressionList(Deque<String> expressionDeque) {
     return new LinkedList<>(expressionDeque);
   }
