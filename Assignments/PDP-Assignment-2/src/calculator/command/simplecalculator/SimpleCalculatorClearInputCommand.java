883fa29998d1690a06a3c436e9aac941e4dd6f87
gajjar.s <gajjar.s@husky.neu.edu>
2018-09-26 17:59:03 -0400

    1. added javadoc for SimpleCalculatorClearInputCommand.java

--- Assignments/PDP-Assignment-2/src/calculator/command/simplecalculator/SimpleCalculatorClearInputCommand.java
+++ Assignments/PDP-Assignment-2/src/calculator/command/simplecalculator/SimpleCalculatorClearInputCommand.java
@@ -19,24 +19,49 @@ import calculator.util.Utils;
  */
 public class SimpleCalculatorClearInputCommand extends AbstractCommand {
 
+  /**
+   * Constructs the object of {@link SimpleCalculatorClearInputCommand}. Its initializes it to the
+   * given values.
+   *
+   * @param expression             current algebraic expression
+   * @param clearInputCharacterSet set of characters supported by the command
+   */
   public SimpleCalculatorClearInputCommand(List<String> expression,
                                            Set<Character> clearInputCharacterSet) {
 
     super(expression, clearInputCharacterSet);
   }
 
+  /**
+   * returns the command name.
+   *
+   * @return the command name
+   */
   @Override
   public CommandName getCommandName() {
     return CommandName.CLEAR;
   }
 
+  /**
+   * Performs the action by returning an empty list.
+   *
+   * @param input input character
+   * @return the algebraic expression generated as result of performing action on the given input
+   */
   @Override
-  public List<String> performAction(char input) throws RuntimeException {
+  public List<String> performAction(char input) {
     Deque<String> expressionDeque = Utils.getExpressionDeque(this.expression);
     expressionDeque.clear();
     return Utils.getExpressionList(expressionDeque);
   }
 
+  /**
+   * Returns a {@link Set} of valid {@link CommandName} that the next input character can represent
+   * after successful execution of the current command.
+   *
+   * @return a {@link Set} of valid {@link CommandName} that the next input character can represent
+   * after successful execution of the current command.
+   */
   @Override
   public Set<CommandName> getNextValidCommands() {
     return Collections.unmodifiableSet(new HashSet<>(Arrays.asList(
