671d3be3a43592ec387ea3b780e83e6c7fa9e2af
gajjar.s <gajjar.s@husky.neu.edu>
2018-09-30 14:00:14 -0400

    added documentation for Question inteface

--- Assignments/PDP-Assignment-3/src/questionnaire/Question.java
+++ Assignments/PDP-Assignment-3/src/questionnaire/Question.java
@@ -1,8 +1,25 @@
 package questionnaire;
 
+/**
+ * This class represents a Question. A Question has its own text which can be retrieved using {@link
+ * Question#getText()} method. For a given {@link Question}, in order to check whether a given
+ * answer is correct or incorrect, {@link Question#eval(String)} method can be used.
+ */
 public interface Question extends Comparable<Question> {
 
+  /**
+   * Return the text of the Question as String.
+   *
+   * @return the text of the {@link Question} as {@link String}
+   */
   String getText();
 
+  /**
+   * Return String "Correct" if the given answer is correct, else return "Incorrect".
+   *
+   * @param answer answer for the given {@link Question}
+   * @return the {@link String} "Correct" if given answer is correct, else return {@link String}
+   * "Incorrect"
+   */
   String eval(String answer);
 }
