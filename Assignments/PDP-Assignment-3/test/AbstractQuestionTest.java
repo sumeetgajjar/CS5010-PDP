5648c60ecb84250a596c7ce25442f700200156c6
gajjar.s <gajjar.s@husky.neu.edu>
2018-09-30 21:36:10 -0400

    added tests to check for hashcode and equalsto

--- Assignments/PDP-Assignment-3/test/AbstractQuestionTest.java
+++ Assignments/PDP-Assignment-3/test/AbstractQuestionTest.java
@@ -23,17 +23,19 @@ public abstract class AbstractQuestionTest {
   public abstract void testOperationOnSameObjectMultipleTimes();
 
   @Test
-  public void testQuestionEqualityAndInEquality() {
+  public abstract void testQuestionObjectInEqualityUsingEquals();
+
+  @Test
+  public abstract void testQuestionObjectInEqualityUsingHashcode();
+
+  @Test
+  public void testQuestionObjectEqualityUsingEquals() {
     Question question1 = getQuestionInstance();
     Question question2 = getQuestionInstance();
     Question question3 = getQuestionInstance();
 
-    Question question4 = getQuestionInstance();
-    Question question5 = getQuestionInstance();
-    Question question6 = getQuestionInstance();
-
     //Checking reflexivity
-    Assert.assertEquals(question1, question1);
+    Assert.assertTrue(question1.equals(question1));
 
     //Checking symmetry
     Assert.assertEquals(question1.equals(question2), question2.equals(question1));
@@ -42,14 +44,16 @@ public abstract class AbstractQuestionTest {
     Assert.assertTrue(question1.equals(question2));
     Assert.assertTrue(question2.equals(question3));
     Assert.assertTrue(question1.equals(question3));
+  }
 
-    //checking same question different answer
-    Assert.assertFalse(question1.equals(question4));
+  @Test
+  public void testQuestionObjectEqualityUsingHashCode() {
+    Question question1 = getQuestionInstance();
+    Question question2 = getQuestionInstance();
+    Question question3 = getQuestionInstance();
 
-    //checking different question same answer
-    Assert.assertFalse(question1.equals(question5));
-
-    //checking different question different answer
-    Assert.assertFalse(question1.equals(question6));
+    Assert.assertEquals(question1.hashCode(), question2.hashCode());
+    Assert.assertEquals(question2.hashCode(), question3.hashCode());
+    Assert.assertEquals(question1.hashCode(), question3.hashCode());
   }
 }
