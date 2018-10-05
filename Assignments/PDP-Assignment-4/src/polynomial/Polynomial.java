a43228c1b327a9c60a05d08e1995e47f89e478ee
gajjar.s <gajjar.s@husky.neu.edu>
2018-10-05 17:46:29 -0400

    assignment 4 init

---
+++ Assignments/PDP-Assignment-4/src/polynomial/Polynomial.java
@@ -0,0 +1,7 @@
+package polynomial;
+
+/**
+ * Created by gajjar.s,on 5:45 PM, 10/5/18
+ */
+public interface Polynomial {
+}
---
+++ Assignments/PDP-Assignment-4/test/PolynomialTest.java
@@ -0,0 +1,6 @@
+/**
+ * Created by gajjar.s,on 5:46 PM, 10/5/18
+ */
+public class PolynomialTest {
+
+}
