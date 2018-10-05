768f78990127ccba686801e548bdaf7d73babf17
gajjar.s <gajjar.s@husky.neu.edu>
2018-10-04 20:25:26 -0400

    added implementation for all methods using lambda's

--- Labs/PDP-Lab3/src/grades/Gradebook.java
+++ Labs/PDP-Lab3/src/grades/Gradebook.java
@@ -2,7 +2,6 @@ package grades;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.OptionalDouble;
 import java.util.stream.Collectors;
 
 public class Gradebook {
@@ -12,10 +11,10 @@ public class Gradebook {
 
   /**
    * Create a new grade book with the given thresholds for letter grades
+   *
    * @param letterGrades the various letter grades
-   * @param thresholds the minimum final score for the corresponding letter
-   *                   grade in the above list. This list is assumed to be in
-   *                   ascending order
+   * @param thresholds   the minimum final score for the corresponding letter grade in the above
+   *                     list. This list is assumed to be in ascending order
    */
   public Gradebook(List<String> letterGrades, List<Double> thresholds) {
     gradebook = new ArrayList<StudentRecord>();
@@ -27,6 +26,7 @@ public class Gradebook {
 
   /**
    * Add a new student to this gradebook.
+   *
    * @param g the new student record to be added
    */
   public void addStudent(StudentRecord g) {
@@ -34,65 +34,82 @@ public class Gradebook {
   }
 
   /**
-   * Given a set of weights, compute the final score of each student and
-   * return as a list.
-   * @param weights the list of weights. The size of this list must be equal
-   *                to the number of assignments in each student's record
+   * Given a set of weights, compute the final score of each student and return as a list.
+   *
+   * @param weights the list of weights. The size of this list must be equal to the number of
+   *                assignments in each student's record
    * @return the final scores of all students as a list
    */
   public List<Double> getFinalScores(List<Double> weights) {
-    return new ArrayList<Double>();
+    return this.gradebook.stream()
+            .map(s -> s.getGrade(weights))
+            .collect(Collectors.toList());
   }
 
   /**
-   * Compute and return the number of students who get the specific letter
-   * grade, when computed using the given weights and the grading scheme in
-   * this grade book.
-   * @param grade the specific letter grade whose frequency is to be counted
-   * @param weights the weights used to compute the final score which maps
-   *                into the letter grade
+   * Compute and return the number of students who get the specific letter grade, when computed
+   * using the given weights and the grading scheme in this grade book.
+   *
+   * @param grade   the specific letter grade whose frequency is to be counted
+   * @param weights the weights used to compute the final score which maps into the letter grade
    * @return the number of students who get the specific letter grade
    */
   public int countLetterGrade(String grade, List<Double> weights) {
-    return 0;
+    return Math.toIntExact(this.gradebook.stream()
+            .map(s -> s.getGrade(weights))
+            .map(this::lettergrade)
+            .filter(grade::equals)
+            .count());
   }
 
   /**
    * Get a list of all student names.
-   * @return a list of all student names. Each element of this list must be a
-   * string that contains the first name and last name, separated by a single
-   * space
+   *
+   * @return a list of all student names. Each element of this list must be a string that contains
+   * the first name and last name, separated by a single space
    */
   public List<String> getStudentNames() {
-    return new ArrayList<String>();
+    return this.gradebook.stream()
+            .map(s -> String.format("%s %s", s.getFirstName(), s.getLastName()))
+            .collect(Collectors.toList());
   }
 
   /**
-   * Given a first name, find the average final score for all students with
-   * this first name, computed using the given weights.
-   * @param name the first name in question
+   * Given a first name, find the average final score for all students with this first name,
+   * computed using the given weights.
+   *
+   * @param name    the first name in question
    * @param weights the weights used to compute the final scores
    * @return the average final score of all students with the given first name
    */
-  public Double averageScoreForName(String name,List<Double> weights) {
-    return 0.0;
+  public Double averageScoreForName(String name, List<Double> weights) {
+    return this.gradebook.stream()
+            .filter(s -> s.getFirstName().equals(name))
+            .mapToDouble(s -> s.getGrade(weights))
+            .average().orElse(0D);
   }
 
   /**
-   * Return the number of students whose final score is above the overall
-   * average final score. The given weights are used to compute the final score.
+   * Return the number of students whose final score is above the overall average final score. The
+   * given weights are used to compute the final score.
+   *
    * @param weights the weights to be used to compute the final scores
-   * @return the number of students whose final score is above the overall
-   * average
+   * @return the number of students whose final score is above the overall average
    */
   public long countAboveAverage(List<Double> weights) {
-    return 0;
+    double averageGrade = this.gradebook.stream()
+            .mapToDouble(s -> s.getGrade(weights))
+            .average().orElse(0D);
+
+    return this.gradebook.stream()
+            .filter(s -> s.getGrade(weights) > averageGrade)
+            .count();
   }
 
   private String lettergrade(Double score) {
 
-    for (int i=0;i<letterGrades.size();i++) {
-      if (score<minThresholds.get(i)) {
+    for (int i = 0; i < letterGrades.size(); i++) {
+      if (score < minThresholds.get(i)) {
         return letterGrades.get(i);
       }
     }
