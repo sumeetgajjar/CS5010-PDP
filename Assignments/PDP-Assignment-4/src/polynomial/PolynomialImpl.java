3923edfe562e8c1e95c761518b22bb6d7e956b7a
gajjar.s <gajjar.s@husky.neu.edu>
2018-10-05 20:08:37 -0400

    added dummy implementation for Polynomial interface

---
+++ Assignments/PDP-Assignment-4/src/polynomial/PolynomialImpl.java
@@ -0,0 +1,54 @@
+package polynomial;
+
+/**
+ * Created by gajjar.s,on 8:05 PM, 10/5/18
+ */
+public class PolynomialImpl implements Polynomial {
+
+  public PolynomialImpl(String polynomialString) throws IllegalArgumentException {
+  }
+
+  public PolynomialImpl() throws IllegalArgumentException {
+  }
+
+
+  @Override
+  public void addTerm(int coefficient, int power) throws IllegalArgumentException {
+
+  }
+
+  @Override
+  public int getDegree() {
+    return 0;
+  }
+
+  @Override
+  public int getCoefficient(int power) {
+    return 0;
+  }
+
+  @Override
+  public double evaluate(double x) {
+    return 0;
+  }
+
+  @Override
+  public Polynomial add(Polynomial polynomial) {
+    return null;
+  }
+
+  @Override
+  public Polynomial derivative() {
+    return null;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    return super.equals(obj);
+  }
+
+  @Override
+  public String toString() {
+    return super.toString();
+  }
+}
