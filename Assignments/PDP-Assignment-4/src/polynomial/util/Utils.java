9e5c4f3d6e0dc3b4b8c75c1bbf02db133528918f
gajjar.s <gajjar.s@husky.neu.edu>
2018-10-06 20:12:40 -0400

    minor refactoring

---
+++ Assignments/PDP-Assignment-4/src/polynomial/util/Utils.java
@@ -0,0 +1,22 @@
+package polynomial.util;
+
+import polynomial.Term;
+
+/**
+ * Created by gajjar.s,on 10:29 AM, 10/6/18
+ */
+public class Utils {
+
+  @SafeVarargs
+  public static <T> T[] getArray(T... objects) {
+    return objects;
+  }
+
+  public static boolean isTermZero(Term term) {
+    return term.getCoefficient() == 0;
+  }
+
+  public static boolean isTermNonZero(Term term) {
+    return !isTermZero(term);
+  }
+}
--- Assignments/PDP-Assignment-4/test/PolynomialTest.java
+++ Assignments/PDP-Assignment-4/test/PolynomialTest.java
@@ -3,7 +3,7 @@ import org.junit.Test;
 
 import polynomial.Polynomial;
 import polynomial.PolynomialImpl;
-import util.Utils;
+import polynomial.util.Utils;
 
 /**
  * Created by gajjar.s,on 5:46 PM, 10/5/18
