4ded10f93e1d1fb972655e062b5f7e1e2736a4e7
gajjar.s <gajjar.s@husky.neu.edu>
2018-10-08 18:40:40 -0400

    1. added documentation for PolynomialTermParser.java

--- Assignments/PDP-Assignment-4/src/polynomial/parser/PolynomialTermParser.java
+++ Assignments/PDP-Assignment-4/src/polynomial/parser/PolynomialTermParser.java
@@ -3,10 +3,18 @@ package polynomial.parser;
 import polynomial.bean.Term;
 
 /**
- * Created by gajjar.s,on 6:18 PM, 10/6/18
+ * This interface represents a Polynomial Term Parser.
  */
 public interface PolynomialTermParser {
 
+  /**
+   * Returns a polynomial {@link Term} by parsing the given string. It throws a {@link
+   * IllegalArgumentException} if the given string is invalid.
+   *
+   * @param isFirstTerm does the given string corresponds to the first term
+   * @param termString  the string to parse
+   * @return the parsed Term
+   * @throws IllegalArgumentException if the given string is invalid
+   */
   Term parsePolynomialTerm(boolean isFirstTerm, String termString) throws IllegalArgumentException;
-
 }
