dfaf9975094ea34aa552bebccfa801776dabcc84
gajjar.s <gajjar.s@husky.neu.edu>
2018-10-08 16:19:37 -0400

    added documentation for Polynomial interface

--- Assignments/PDP-Assignment-4/src/polynomial/Polynomial.java
+++ Assignments/PDP-Assignment-4/src/polynomial/Polynomial.java
@@ -1,7 +1,7 @@
 package polynomial;
 
 /**
- * Created by gajjar.s,on 5:45 PM, 10/5/18
+ * This interface represents a Polynomial and all operations that can be performed on a polynomial.
  */
 public interface Polynomial {
 
