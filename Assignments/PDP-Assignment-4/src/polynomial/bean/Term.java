3c1241625c11d47c4282a35a2a6ec18d43dc5123
gajjar.s <gajjar.s@husky.neu.edu>
2018-10-08 16:12:16 -0400

    added documentation for Term class

--- Assignments/PDP-Assignment-4/src/polynomial/bean/Term.java
+++ Assignments/PDP-Assignment-4/src/polynomial/bean/Term.java
@@ -3,13 +3,22 @@ package polynomial.bean;
 import java.util.Objects;
 
 /**
- * Created by gajjar.s,on 4:26 PM, 10/6/18
+ * This class represents a Term of the Polynomial. A {@link Term} has a coefficient and a power. The
+ * power of the term cannot be less than zero. If an attempt is made to create a term with power
+ * less than 0 than it will throw an {@link IllegalArgumentException}.
  */
 public class Term {
 
   private final int coefficient;
   private final int power;
 
+  /**
+   * Constructs a Term with given coefficient and power.
+   *
+   * @param coefficient the coefficient of the term
+   * @param power       the power of the term
+   * @throws IllegalArgumentException if the given power is less than zero
+   */
   public Term(int coefficient, int power) throws IllegalArgumentException {
 
     if (power < 0) {
@@ -20,10 +29,21 @@ public class Term {
     this.power = power;
   }
 
+  /**
+   * Constructs a new {@link Term} object with coefficient and power of the given {@link Term}.
+   *
+   * @param term the term to be copied
+   */
   public Term(Term term) {
     this(term.getCoefficient(), term.getPower());
   }
 
+  /**
+   * Checks if given obj is equal to this Term.
+   *
+   * @param obj the object to be compared
+   * @return true if given obj is equal to this Term, false otherwise
+   */
   @Override
   public boolean equals(Object obj) {
     if (this == obj) {
@@ -35,10 +55,14 @@ public class Term {
     }
 
     Term that = (Term) obj;
-    return this.coefficient == that.coefficient
-            && this.power == that.power;
+    return (this.coefficient == that.coefficient) && (this.power == that.power);
   }
 
+  /**
+   * Generates the hashCode of this Term using coefficient and power.
+   *
+   * @return the hashCode of this object
+   */
   @Override
   public int hashCode() {
     return Objects.hash(this.coefficient, this.power);
@@ -52,28 +76,67 @@ public class Term {
     return String.format("%s%d%s", sign, this.coefficient, variableString);
   }
 
+  /**
+   * Returns the evaluation of this Term for given value of x.
+   *
+   * @param x the given value
+   * @return the evaluation of the term for given value of x
+   */
   public double evaluate(double x) {
     return coefficient * (Math.pow(x, power));
   }
 
+  /**
+   * Returns a new {@link Term} which is the Derivative of this {@link Term}. It throws a {@link
+   * ArithmeticException} if the coefficient overflows while differentiating the term.
+   *
+   * @return a new {@link Term} which is the Derivative of this {@link Term}
+   * @throws ArithmeticException if the coefficient overflows while calculating the derivative
+   */
   public Term differentiate() throws ArithmeticException {
     int powerOfDerivative = Math.max(0, this.power - 1);
     int coefficientOfDerivative = Math.multiplyExact(this.power, this.coefficient);
     return new Term(coefficientOfDerivative, powerOfDerivative);
   }
 
+  /**
+   * Returns the coefficient of this term.
+   *
+   * @return the coefficient of this term
+   */
   public int getCoefficient() {
     return coefficient;
   }
 
+  /**
+   * Returns the power of this term.
+   *
+   * @return the power of this term
+   */
   public int getPower() {
     return power;
   }
 
+  /**
+   * Returns a new {@link Term} with zero coefficient and zero power.
+   *
+   * @return a new {@link Term} with zero coefficient and zero power
+   */
   public static Term getZeroTerm() {
     return new Term(0, 0);
   }
 
+  /**
+   * Returns the result of addition of given terms. Throws an {@link IllegalArgumentException} if
+   * the power of the given terms is not same. Throws {@link ArithmeticException} if an overflow
+   * occurs while adding the given terms.
+   *
+   * @param term1 first term to be added
+   * @param term2 second term to be added
+   * @return the addition of the given terms
+   * @throws IllegalArgumentException if the power of the given terms is not same
+   * @throws ArithmeticException      if an overflow occurs while adding the given terms
+   */
   public static Term addTwoTerms(Term term1, Term term2)
           throws IllegalArgumentException, ArithmeticException {
 
