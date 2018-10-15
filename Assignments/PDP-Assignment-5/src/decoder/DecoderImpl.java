3aa349df8bf0a7474e422b04691c4572f60ff7c7
gajjar.s <gajjar.s@husky.neu.edu>
2018-10-15 11:49:00 -0400

    1. added dummy implementation for Decoder Interface

---
+++ Assignments/PDP-Assignment-5/src/decoder/DecoderImpl.java
@@ -0,0 +1,31 @@
+package decoder;
+
+/**
+ * Created by gajjar.s, on 11:46 AM, 10/15/18
+ */
+public class DecoderImpl implements Decoder {
+
+  public DecoderImpl(String codingSymbols) {
+
+  }
+
+  @Override
+  public void addCode(char symbol, String code) throws IllegalArgumentException {
+
+  }
+
+  @Override
+  public String decode(String encodedMessage) throws IllegalStateException {
+    return null;
+  }
+
+  @Override
+  public String allCodes() {
+    return null;
+  }
+
+  @Override
+  public boolean isCodeComplete() {
+    return false;
+  }
+}
