88289344b4f00700c36ff2719585dd91916a3f3d
gajjar.s <gajjar.s@husky.neu.edu>
2018-10-18 15:51:37 -0400

    added documentation for the encoder

--- Assignments/PDP-Assignment-5/src/encoder/Encoder.java
+++ Assignments/PDP-Assignment-5/src/encoder/Encoder.java
@@ -4,11 +4,28 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Created by gajjar.s, on 9:28 PM, 10/17/18
+ * This interface represents a Encoder and all operations that can be performed on an Encoder.
  */
 public interface Encoder {
 
+  /**
+   * Given a message and coding Symbols, returns the coding table for each symbol in message.
+   *
+   * @param codingSymbols the valid list of coding symbols
+   * @param message       the message to generate coding table for
+   * @return the coding table for each symbol in message
+   */
   Map<Character, String> generateCodingTable(List<Character> codingSymbols, String message);
 
+  /**
+   * Given a coding table and a message, returns the encoded form of the message. The encoding is
+   * done using the coding table provided. If encoding fails due any given reason then the encoder
+   * throws an {@link IllegalStateException}.
+   *
+   * @param codingTable the coding table for the message
+   * @param message     the message to encode
+   * @return the encoded form of the message
+   * @throws IllegalStateException if the encoding fails for any reason
+   */
   String encode(Map<Character, String> codingTable, String message) throws IllegalStateException;
 }
