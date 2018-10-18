0ddcb5ca2b54812970f41b39c2e90e7175bcac40
gajjar.s <gajjar.s@husky.neu.edu>
2018-10-18 13:37:07 -0400

    1. added test to test encoder and decoder interactively

--- Assignments/PDP-Assignment-5/test/EncoderDecoderTest.java
+++ Assignments/PDP-Assignment-5/test/EncoderDecoderTest.java
@@ -1,5 +1,35 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Map;
+
+import decoder.Decoder;
+import decoder.DecoderImpl;
+import encoder.Encoder;
+import encoder.HuffmanEncoder;
+import util.Utils;
+
 /**
- * Created by gajjar.s, on 1:19 AM, 10/18/18
+ * A Junit class to Interactively Encoder and Decoder.
  */
 public class EncoderDecoderTest {
+
+  @Test
+  public void testEncoderDecoderInteractively() {
+    Encoder<Character> encoder = new HuffmanEncoder();
+    String codingSymbols = "01";
+    String originalMessage = "Robots are gonna take over the world, I am just playing my part";
+    Map<Character, String> codingTable = encoder.generateCodingTable(
+            Utils.convertStringToCharacterArray(codingSymbols), originalMessage);
+
+    String encodedMessage = encoder.encode(codingTable, originalMessage);
+
+    Decoder decoder = new DecoderImpl(codingSymbols);
+    for (Map.Entry<Character, String> entry : codingTable.entrySet()) {
+      decoder.addCode(entry.getKey(), entry.getValue());
+    }
+
+    String decodedMessage = decoder.decode(encodedMessage);
+    Assert.assertEquals(originalMessage, decodedMessage);
+  }
 }
