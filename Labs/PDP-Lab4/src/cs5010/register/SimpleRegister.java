b7caa8c190b5c1c927042844c4727e6cf120f581
gajjar.s <gajjar.s@husky.neu.edu>
2018-10-26 13:43:35 -0400

    1. minor refactoring

--- Labs/PDP-Lab4/src/cs5010/register/SimpleRegister.java
+++ Labs/PDP-Lab4/src/cs5010/register/SimpleRegister.java
@@ -1,7 +1,59 @@
 package cs5010.register;
 
+import java.util.Map;
+
 /**
  * Created by gajjar.s, on 1:37 PM, 10/26/18
  */
-public class SimpleRegister {
+public class SimpleRegister implements CashRegister {
+
+  @Override
+  public void addPennies(int num) {
+
+  }
+
+  @Override
+  public void addNickels(int num) {
+
+  }
+
+  @Override
+  public void addDimes(int num) {
+
+  }
+
+  @Override
+  public void addQuarters(int num) {
+
+  }
+
+  @Override
+  public void addOnes(int num) {
+
+  }
+
+  @Override
+  public void addFives(int num) {
+
+  }
+
+  @Override
+  public void addTens(int num) {
+
+  }
+
+  @Override
+  public Map<Integer, Integer> withdraw(int dollars, int cents) throws InsufficientCashException {
+    return null;
+  }
+
+  @Override
+  public Map<Integer, Integer> getContents() {
+    return null;
+  }
+
+  @Override
+  public String getAuditLog() {
+    return null;
+  }
 }
---
+++ Labs/PDP-Lab4/test/SimpleRegisterTest.java
@@ -0,0 +1,5 @@
+/**
+ * Created by gajjar.s, on 1:42 PM, 10/26/18
+ */
+public class SimpleRegisterTest {
+}
