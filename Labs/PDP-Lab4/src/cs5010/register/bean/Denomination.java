d1ac3902de7f622105602d663235bce83abb1a8c
gajjar.s <gajjar.s@husky.neu.edu>
2018-10-27 12:56:40 -0400

    1. refactored documentation for Denomination.java

--- Labs/PDP-Lab4/src/cs5010/register/bean/Denomination.java
+++ Labs/PDP-Lab4/src/cs5010/register/bean/Denomination.java
@@ -25,18 +25,18 @@ public enum Denomination {
   }
 
   /**
-   * Return the number of pennies in the given denomination count.
+   * Returns the number of pennies for the given denomination count in this {@link Denomination}.
    *
-   * @param count the denomination count
-   * @return the number of pennies in the given denomination count
+   * @param denominationCount the denomination count
+   * @return the number of pennies for the given denomination count in this {@link Denomination}
    */
-  public int getPennies(int count) {
-    return count * this.numberOfPenniesInDenomination;
+  public int getPennies(int denominationCount) {
+    return denominationCount * this.numberOfPenniesInDenomination;
   }
 
   /**
-   * Return the maximum equivalent count of this denomination for given value of pennies. For e.g if
-   * number of pennies is 17 and this is {@link Denomination#TENS} then it will return 1. If the
+   * Returns the maximum equivalent count of this denomination for given value of pennies. For e.g
+   * if number of pennies is 17 and this is {@link Denomination#TENS} then it will return 1. If the
    * given amount is 9 and this is {@link Denomination#TENS} then it will return 0. If the given
    * amount is 20 and this is {@link Denomination#TENS} then it will return 2.
    *
@@ -48,15 +48,15 @@ public enum Denomination {
   }
 
   /**
-   * Return the maximum equivalent dollar count for the given count of this denomination. For e.g if
-   * given count is 1700 and this is {@link Denomination#PENNIES} then it will return 17. If the
+   * Returns the maximum equivalent dollar count for the given count of this denomination. For e.g
+   * if given count is 1700 and this is {@link Denomination#PENNIES} then it will return 17. If the
    * given amount is 9 and this is {@link Denomination#PENNIES} then it will return 0. If the given
    * amount is 200 and this is {@link Denomination#PENNIES} then it will return 2.
    *
-   * @param count the denomination count
+   * @param denominationCount the denomination count
    * @return the maximum equivalent dollar count for the given count of this denomination
    */
-  public int getDollars(int count) {
-    return ONES.getDenominationCount(getPennies(count));
+  public int getDollars(int denominationCount) {
+    return ONES.getDenominationCount(getPennies(denominationCount));
   }
 }
