161d45fc7e6a10ff837aaa97e355260b39890953
gajjar.s <gajjar.s@husky.neu.edu>
2018-11-01 18:21:48 -0400

    added test class

---
+++ Labs/PDP-Lab5/test/ImmutableListTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * A Junit class to test Immutable List.
+ */
+public class ImmutableListTest {
+
+  @Test
+  public void testInitializationOfImmutableList() {
+
+  }
+}
