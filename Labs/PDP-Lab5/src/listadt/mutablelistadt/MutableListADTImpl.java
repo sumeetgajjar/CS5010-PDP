c7f638ca234d8b965b4feb8ab4448a55b5677dbf
gajjar.s <gajjar.s@husky.neu.edu>
2018-11-02 19:31:14 -0400

    1. added documentation for MutableListADTImpl.java

--- Labs/PDP-Lab5/src/listadt/mutablelistadt/MutableListADTImpl.java
+++ Labs/PDP-Lab5/src/listadt/mutablelistadt/MutableListADTImpl.java
@@ -9,10 +9,19 @@ import listadt.immutablelistadt.ImmutableListADTBuilder;
 import listadt.immutablelistadt.ImmutableListADTImpl;
 
 /**
- * Created by gajjar.s, on 1:22 PM, 11/2/18
+ * This class represents a Mutable List. It extends {@link ListADTImpl} and it implements {@link
+ * MutableListADT}.
  */
 public class MutableListADTImpl<T> extends ListADTImpl<T> implements MutableListADT<T> {
 
+  /**
+   * A general purpose map higher order function on this {@link MutableListADT}, that returns the
+   * corresponding {@link MutableListADT} of type R.
+   *
+   * @param converter the function that converts T into R
+   * @param <R>       the type of data in the resulting list
+   * @return the resulting list that is identical in structure to this list, but has data of type R
+   */
   @Override
   public <R> MutableListADT<R> map(Function<T, R> converter) {
     ListADT<R> mappedList = super.map(converter);
@@ -25,6 +34,18 @@ public class MutableListADTImpl<T> extends ListADTImpl<T> implements MutableList
     return mappedMutableListADT;
   }
 
+  /**
+   * Returns a {@link ImmutableListADT} containing all elements of this {@link MutableListADT}.
+   * Modifying the contents of this {@link MutableListADT} will not modify the previously created
+   * {@link ImmutableListADT} of this {@link MutableListADT}. For e.g. if
+   * <code>mutableList=[1,2,3,4]</code> and we create {@link ImmutableListADT} of
+   * "mutableList", <code>immutableListBeforeUpdate = mutableList.getImmutableListADT()</code> then
+   * immutableListBeforeUpdate=[1,2,3,4]. Now lets add "5" at the end of the mutableList, so
+   * mutableList = [1,2,3,4,5]. The immutableListBeforeUpdate will contain only 4 elements i.e.
+   * immutableListBeforeUpdate=[1,2,3,4].
+   *
+   * @return a {@link ImmutableListADT} containing all elements of this {@link MutableListADT}
+   */
   @Override
   public ImmutableListADT<T> getImmutableListADT() {
     ImmutableListADTBuilder<T> builder = ImmutableListADTImpl.getBuilder();
