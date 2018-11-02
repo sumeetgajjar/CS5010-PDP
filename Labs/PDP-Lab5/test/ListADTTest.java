b2119bb62c4230f1fac38cc471fe79c36ec598dd
gajjar.s <gajjar.s@husky.neu.edu>
2018-11-02 14:09:55 -0400

    1. added testMutatingMutableListObtainedFromImmutableListDoesNotMutateImmutableList

--- Labs/PDP-Lab5/test/ListADTTest.java
+++ Labs/PDP-Lab5/test/ListADTTest.java
@@ -39,6 +39,62 @@ public class ListADTTest {
   }
 
   @Test
+  public void testMutatingMutableListObtainedFromImmutableListDoesNotMutateImmutableList() {
+    ImmutableListADT<String> immutableListADT = new ImmutableListADTImpl<>(getListADT());
+    MutableListADT<String> mutableListAdt = immutableListADT.getMutableListAdt();
+
+    mutableListAdt.add(4, "5");
+    Assert.assertEquals("5", mutableListAdt.get(4));
+    Assert.assertEquals(5, mutableListAdt.getSize());
+
+    Assert.assertEquals(4, immutableListADT.getSize());
+
+    try {
+      immutableListADT.get(4);
+      Assert.fail("should have failed");
+    } catch (IllegalArgumentException e) {
+      Assert.assertEquals("Invalid index", e.getMessage());
+    }
+
+    mutableListAdt.addFront("-1");
+    Assert.assertEquals("-1", mutableListAdt.get(0));
+    Assert.assertEquals(6, mutableListAdt.getSize());
+
+    String expectedFirstElementOfImmutableList = "0";
+    Assert.assertEquals(expectedFirstElementOfImmutableList, immutableListADT.get(0));
+    Assert.assertEquals(4, immutableListADT.getSize());
+
+    try {
+      immutableListADT.get(4);
+      Assert.fail("should have failed");
+    } catch (IllegalArgumentException e) {
+      Assert.assertEquals("Invalid index", e.getMessage());
+    }
+
+    mutableListAdt.addBack("6");
+    Assert.assertEquals("6", mutableListAdt.get(6));
+    Assert.assertEquals(7, mutableListAdt.getSize());
+
+    String expectedLastElementOfImmutableList = "4";
+    Assert.assertEquals(expectedLastElementOfImmutableList, immutableListADT.get(3));
+    Assert.assertEquals(4, immutableListADT.getSize());
+
+    try {
+      mutableListAdt.get(4);
+      Assert.fail("should have failed");
+    } catch (IllegalArgumentException e) {
+      Assert.assertEquals("Invalid index", e.getMessage());
+    }
+
+    mutableListAdt.remove("0");
+    Assert.assertEquals(6, mutableListAdt.getSize());
+
+    expectedFirstElementOfImmutableList = "0";
+    Assert.assertEquals(expectedFirstElementOfImmutableList, immutableListADT.get(0));
+    Assert.assertEquals(4, immutableListADT.getSize());
+  }
+
+  @Test
   public void testAddFailureOnImmutableList() {
     ImmutableListADT<String> immutableListADT = new ImmutableListADTImpl<>(getListADT());
     Assert.assertEquals("1", immutableListADT.get(0));
