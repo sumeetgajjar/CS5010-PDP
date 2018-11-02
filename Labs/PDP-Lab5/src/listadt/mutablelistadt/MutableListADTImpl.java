d7ffb1202a843279d0c0f6e84f1d2e9fcb2d4e71
gajjar.s <gajjar.s@husky.neu.edu>
2018-11-02 18:13:52 -0400

    1. added implementation for MutableListImpl

--- Labs/PDP-Lab5/src/listadt/mutablelistadt/MutableListADTImpl.java
+++ Labs/PDP-Lab5/src/listadt/mutablelistadt/MutableListADTImpl.java
@@ -2,8 +2,11 @@ package listadt.mutablelistadt;
 
 import java.util.function.Function;
 
+import listadt.ListADT;
 import listadt.ListADTImpl;
 import listadt.immutablelistadt.ImmutableListADT;
+import listadt.immutablelistadt.ImmutableListADTBuilder;
+import listadt.immutablelistadt.ImmutableListADTImpl;
 
 /**
  * Created by gajjar.s, on 1:22 PM, 11/2/18
@@ -12,11 +15,23 @@ public class MutableListADTImpl<T> extends ListADTImpl<T> implements MutableList
 
   @Override
   public <R> MutableListADT<R> map(Function<T, R> converter) {
-    return null;
+    ListADT<R> mappedList = super.map(converter);
+
+    MutableListADTImpl<R> mappedMutableListADT = new MutableListADTImpl<>();
+    int size = mappedList.getSize();
+    for (int i = 0; i < size; i++) {
+      mappedMutableListADT.addBack(mappedList.get(i));
+    }
+    return mappedMutableListADT;
   }
 
   @Override
   public ImmutableListADT<T> getImmutableListADT() {
-    return null;
+    ImmutableListADTBuilder<T> builder = ImmutableListADTImpl.getBuilder();
+    int size = this.getSize();
+    for (int i = 0; i < size; i++) {
+      builder.add(this.get(i));
+    }
+    return builder.build();
   }
 }
