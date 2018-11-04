c0012efcfee1ee253ff6319080a1568c93688be6
gajjar.s <gajjar.s@husky.neu.edu>
2018-11-03 20:31:14 -0400

    1. added test to check passing null values to builder

--- Labs/PDP-Lab5/test/ImmutableListADTTest.java
+++ Labs/PDP-Lab5/test/ImmutableListADTTest.java
@@ -237,6 +237,30 @@ public class ImmutableListADTTest {
   }
 
   @Test
+  public void testConstructingImmutableListUsingNullList() {
+    try {
+      ListADTFactory.getNewImmutableListADTBuilder()
+              .addAll(null);
+      Assert.fail("should have failed");
+    } catch (IllegalArgumentException e) {
+      Assert.assertEquals("given list cannot be null", e.getMessage());
+    }
+  }
+
+  @Test
+  public void testConstructingImmutableListWithNullElement() {
+    ImmutableListADT<String> immutableListADT =
+            ListADTFactory.<String>getNewImmutableListADTBuilder()
+                    .add("1")
+                    .add(null)
+                    .add("2")
+                    .build();
+
+    Assert.assertNull(immutableListADT.get(1));
+    Assert.assertEquals("2", immutableListADT.get(2));
+  }
+
+  @Test
   public void testConstructingImmutableListUsingBuilder() {
 
     ListADT<Integer> listADT = new ListADTImpl<>();
