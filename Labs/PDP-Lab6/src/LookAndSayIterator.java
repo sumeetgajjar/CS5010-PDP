d8691c6e42f5b18a4a8f0f29ac28fe9a32b94814
gajjar.s <gajjar.s@husky.neu.edu>
2018-11-10 12:45:06 -0500

    1. added documentation for methods 2. added tests

--- Labs/PDP-Lab6/src/LookAndSayIterator.java
+++ Labs/PDP-Lab6/src/LookAndSayIterator.java
@@ -7,14 +7,37 @@ public class LookAndSayIterator implements RIterator<BigInteger> {
 
   private static final BigInteger LARGEST_END_BIG_INTEGER = get100DigitRepeatingString(9);
 
-  public LookAndSayIterator(BigInteger seed, BigInteger end) {
+
+  /**
+   * Constructs a {@link LookAndSayIterator} object with the given params. It throws an {@link
+   * IllegalArgumentException} if the number of continuous digits in the given seed is greater than
+   * 9 or if the seed is not a positive number, or is not less than the end.
+   *
+   * @param seed the given seed
+   * @param end  the given end number
+   * @throws IllegalArgumentException if the given seed is invalid
+   */
+  public LookAndSayIterator(BigInteger seed, BigInteger end) throws IllegalArgumentException {
 
   }
 
-  public LookAndSayIterator(BigInteger seed) {
+  /**
+   * Constructs a {@link LookAndSayIterator} object with the given params. The end value will be a
+   * number with 100 9s (the largest 100 digit number). It throws an {@link
+   * IllegalArgumentException} if the number of continuous digits in the given seed is greater than
+   * 9.
+   *
+   * @param seed the given seed
+   * @throws IllegalArgumentException if the given seed is invalid
+   */
+  public LookAndSayIterator(BigInteger seed) throws IllegalArgumentException {
 
   }
 
+  /**
+   * Constructs a {@link LookAndSayIterator} object with the given params. The seed value will be 1.
+   * The end value will be a number with 100 9s (the largest 100 digit number).
+   */
   public LookAndSayIterator() {
   }
 
@@ -23,11 +46,27 @@ public class LookAndSayIterator implements RIterator<BigInteger> {
     return null;
   }
 
+  /**
+   * Returns true if it is possible to go back one step, false otherwise. It cannot go one step
+   * behind in following cases
+   * <ul>
+   * <li>If the current number contains odd number of digits</li>
+   * <li>If the </li>
+   * </ul>
+   *
+   * @return true if it is possible to go back one step, false otherwise
+   */
   @Override
   public boolean hasPrevious() {
     return false;
   }
 
+  /**
+   * Returns true if the next number to be returned is still lesser than end value, false
+   * otherwise.
+   *
+   * @return true if the next number to be returned is still lesser than end value, false otherwise
+   */
   @Override
   public boolean hasNext() {
     return false;
--- Labs/PDP-Lab6/test/LookAndSayIteratorTest.java
+++ Labs/PDP-Lab6/test/LookAndSayIteratorTest.java
@@ -48,6 +48,30 @@ public class LookAndSayIteratorTest {
   }
 
   @Test
+  public void testInvalidSeedValue() {
+    try {
+      new LookAndSayIterator(new BigInteger("0"));
+      Assert.fail("should have failed");
+    } catch (IllegalArgumentException e) {
+      Assert.assertEquals("invalid seed value", e.getMessage());
+    }
+
+    try {
+      new LookAndSayIterator(new BigInteger("-1"));
+      Assert.fail("should have failed");
+    } catch (IllegalArgumentException e) {
+      Assert.assertEquals("invalid seed value", e.getMessage());
+    }
+
+    try {
+      new LookAndSayIterator(BigInteger.TEN, BigInteger.ONE);
+      Assert.fail("should have failed");
+    } catch (IllegalArgumentException e) {
+      Assert.assertEquals("invalid seed value", e.getMessage());
+    }
+  }
+
+  @Test
   public void testEndValueWithDoubleDigitFrequencyWorks() {
     RIterator<BigInteger> rIterator = new LookAndSayIterator(new BigInteger("111111111"),
             new BigInteger("1111111111"));
@@ -56,8 +80,31 @@ public class LookAndSayIteratorTest {
 
   @Test
   public void testNextWorks() {
-    RIterator<BigInteger> rIterator = new LookAndSayIterator(BigInteger.ONE, BigInteger.TEN);
-    Assert.assertEquals(BigInteger.ONE, rIterator.next());
-    Assert.assertEquals(BigInteger.ONE, rIterator.next());
+    RIterator<BigInteger> rIterator = new LookAndSayIterator(BigInteger.ONE, new BigInteger("100"));
+    Assert.assertTrue(rIterator.hasNext());
+    Assert.assertEquals(new BigInteger("11"), rIterator.next());
+
+    Assert.assertTrue(rIterator.hasNext());
+    Assert.assertEquals(new BigInteger("21"), rIterator.next());
+
+    Assert.assertFalse(rIterator.hasNext());
+    Assert.assertEquals(new BigInteger("21"), rIterator.next());
+    Assert.assertFalse(rIterator.hasNext());
+    Assert.assertEquals(new BigInteger("21"), rIterator.next());
+    Assert.assertFalse(rIterator.hasNext());
+    Assert.assertEquals(new BigInteger("21"), rIterator.next());
+  }
+
+  @Test
+  public void testPrevWorks() {
+    RIterator<BigInteger> rIterator = new LookAndSayIterator(new BigInteger("213"));
+    Assert.assertTrue(rIterator.hasNext());
+    Assert.assertEquals(new BigInteger("121113"), rIterator.next());
+
+    Assert.assertTrue(rIterator.hasPrevious());
+    Assert.assertEquals(new BigInteger("213"), rIterator.prev());
+
+    Assert.assertFalse(rIterator.hasPrevious());
+    Assert.assertEquals(new BigInteger("213"), rIterator.prev());
   }
 }
