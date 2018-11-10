481fa279f82b6ba6a95a5c013cddc47098d83440
gajjar.s <gajjar.s@husky.neu.edu>
2018-11-09 20:24:24 -0500

    1. added interface RIterator.java

---
+++ Labs/PDP-Lab6/src/RIterator.java
@@ -0,0 +1,11 @@
+import java.util.Iterator;
+
+/**
+ * Created by gajjar.s, on 8:22 PM, 11/9/18
+ */
+public interface RIterator<T> extends Iterator<T> {
+
+  T prev();
+
+  boolean hasPrevious();
+}
