dbc0a63ad60208dd0a2894d33f19df09d86823b3
gajjar.s <gajjar.s@husky.neu.edu>
2018-11-10 18:01:48 -0500

    1. added documentation for RIterator.java

--- Labs/PDP-Lab6/src/lookandsay/RIterator.java
+++ Labs/PDP-Lab6/src/lookandsay/RIterator.java
@@ -3,11 +3,22 @@ package lookandsay;
 import java.util.Iterator;
 
 /**
- * Created by gajjar.s, on 8:22 PM, 11/9/18
+ * This interface represents a generic reversible Iterator. It extends {@link Iterator}. It has 2
+ * methods to check and go backwards while iterating.
  */
 public interface RIterator<T> extends Iterator<T> {
 
+  /**
+   * Returns the next element in the iteration.
+   *
+   * @return the previous element in the iteration
+   */
   T prev();
 
+  /**
+   * Returns true if there exists a previous element to iterate on, false otherwise.
+   *
+   * @return true if there exists a previous element to iterate on, false otherwise
+   */
   boolean hasPrevious();
 }
